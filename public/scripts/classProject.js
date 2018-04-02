0a948ce471d81908eafcd6bb390d838953e881a8
Chris Neal <chrisvneal@gmail.com>
2018-04-01 19:49:54 -1000

    Redo menu functionality

--- public/scripts/classProject.js
+++ public/scripts/classProject.js
@@ -1,34 +1,31 @@
 //Put your JavaScript code in this file
 //master
 
-$(document).ready(function(){
-// Add <hr> after ecery section eleemnt in the resource group stack
-$('.resources-group section').after("<hr>");
+$(document).ready(function() {
+  // Add <hr> after ecery section eleemnt in the resource group stack
+  $('.resources-group section').after("<hr>");
 
-$('<a href="#resources">back to top</a>').insertBefore('.resources-group hr');
-
-//*************************************************************
-//	Hamburger menu-icon
-//*************************************************************
-// Rename X var to something more useful
-	$(".menu-icon").on("click", function(){
-		// $(this).css("font-size", "15px");
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
-        }
-    });
-//*************************************************************
-// End of Hamburger********************************************
-//*************************************************************
+  $('<a href="#resources">back to top</a>').insertBefore('.resources-group hr');
 
 
+
+
+
+
+  $(".menu-icon").on("click", function() {
+
+    if ($('.small-hide').hasClass('small-hide')) {
+      $('.small-hide').removeClass('small-hide');
+    } else {
+      $("#mainHeader a ~ a:not('.menu-icon')").addClass('small-hide');
+    }
+
+
+
+  });
+  // end menu-icon function
 
 
 
 
 });
-
-//master
