8a15cd983974c35ee4489661c976cf56596b9163
leslie <leslie.qn@gmail.com>
2018-04-09 18:57:28 -0700

    add free/paid icons
    Add new column to resources table indicating if resource is free or requires payment

--- gtable.js
+++ gtable.js
@@ -14,21 +14,47 @@ function init() {
 
 function resourceData(data, tabletop) {
   for (var i = 0; i < data.length; i++) {
-    $('.post').append(
-      '<tr>' +
-      '<td id="col1" scope="row">' +
-      data[i].Resource +
-      '</td>' +
-      '<td class="text" id = "' + data[i].Type + '">' +
-      data[i].Type +
-      '</td>' +
-      '<td class="text">' +
-      data[i].Description +
-      '</td>' +
-      '<td id="address"><a href ="' +
-      data[i].Link + '" target="_blank">Check it out!</a> ' +
-      '</td>' +
-      '</tr>' );
+    if (data[i].Free == 0) {
+      $('.post').append(
+        '<tr>' +
+        '<td id="col1" scope="row">' +
+        data[i].Resource +
+        '</td>' +
+        '<td class="text">' +
+        '<span class="fa-stack fa-2x"><i class="fa fa-usd fa-stack-1x"></i><i class="fa fa-circle-thin fa-stack-2x"></i></span>' +
+        '</td>' +
+        '<td class="text" id = "' + data[i].Type + '">' +
+        data[i].Type +
+        '</td>' +
+        '<td class="text">' +
+        data[i].Description +
+        '</td>' +
+        '<td id="address"><a href ="' +
+        data[i].Link + '" target="_blank">Check it out!</a> ' +
+        '</td>' +
+        '</tr>' );
+    }
+    else if (data[i].Free == 1) {
+      $('.post').append(
+        '<tr>' +
+        '<td id="col1" scope="row">' +
+        data[i].Resource +
+        '</td>' +
+        '<td class="text">' +
+        '<span class="fa-stack fa-2x"><i class="fa fa-usd fa-stack-1x"></i><i class="fa fa-ban fa-stack-2x" style="color:Red"></i></span>' +
+        '</td>' +
+        '<td class="text" id = "' + data[i].Type + '">' +
+        data[i].Type +
+        '</td>' +
+        '<td class="text">' +
+        data[i].Description +
+        '</td>' +
+        '<td id="address"><a href ="' +
+        data[i].Link + '" target="_blank">Check it out!</a> ' +
+        '</td>' +
+        '</tr>' );
+    }
+
   }
 
 }
