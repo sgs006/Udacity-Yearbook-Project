68fa884f0be4f688b3ca60cc7528b4fa7868381b
Forrest Walker <me@forrestwalker.net>
2018-04-13 19:50:04 -0500

    Order table by button click
    Rearranges the resource table once a button is clicked instead of jumping to that desired section. Trying to keep user at top of page for more friendly navigation. Also cleaned up resourceData a tad

--- gtable.js
+++ gtable.js
@@ -5,59 +5,52 @@ var resourceSpreadsheet = '15_xFyNCeTMRxSbdJuqhezhw1Ahb69NplZG8iNjs9AII';
 
 
 function init() {
-  Tabletop.init({
-    key: resourceSpreadsheet,
-    callback: resourceData,
-    simpleSheet: true
-  });
+	Tabletop.init({
+		key: resourceSpreadsheet,
+		callback: resourceData,
+		simpleSheet: true
+	});
 }
 
 function resourceData(data, tabletop) {
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].Free == 0) {
-      $('.post').append(
-        '<tr>' +
-        '<td id="col1" scope="row">' +
-        data[i].Resource +
-        '</td>' +
-        '<td class="text">' +
-        '<span class="fa-stack fa-2x"><i class="fa fa-usd fa-stack-1x"></i><i class="fa fa-circle-thin fa-stack-2x"></i></span>' +
-        '</td>' +
-        '<td class="text" id = "' + data[i].Type + '">' +
-        data[i].Type +
-        '</td>' +
-        '<td class="text">' +
-        data[i].Description +
-        '</td>' +
-        '<td id="address"><a href ="' +
-        data[i].Link + '" target="_blank">Check it out!</a> ' +
-        '</td>' +
-        '</tr>' );
-    }
-    else if (data[i].Free == 1) {
-      $('.post').append(
-        '<tr>' +
-        '<td id="col1" scope="row">' +
-        data[i].Resource +
-        '</td>' +
-        '<td class="text">' +
-        '<span class="fa-stack fa-2x"><i class="fa fa-usd fa-stack-1x"></i><i class="fa fa-ban fa-stack-2x" style="color:Red"></i></span>' +
-        '</td>' +
-        '<td class="text" id = "' + data[i].Type + '">' +
-        data[i].Type +
-        '</td>' +
-        '<td class="text">' +
-        data[i].Description +
-        '</td>' +
-        '<td id="address"><a href ="' +
-        data[i].Link + '" target="_blank">Check it out!</a> ' +
-        '</td>' +
-        '</tr>' );
-    }
+	for (let i = 0; i < data.length; i++) {
+		$('.post').append(
+			'<tr>' +
+			'<td id="col1" scope="row">' +
+			data[i].Resource +
+			'</td>' +
+			'<td class="text">' +
+			'<span class="fa-stack fa-2x"><i class="fa fa-usd fa-stack-1x"></i><i class="fa fa-circle-thin fa-stack-2x"' + ((data[i].Free === 1) ? 'style="color:red"' : 'style="color:inherit"') + '></i></span>' +
+			'</td>' +
+			'<td class="text" id = "' + data[i].Type + '">' +
+			data[i].Type +
+			'</td>' +
+			'<td class="text">' +
+			data[i].Description +
+			'</td>' +
+			'<td id="address"><a href ="' +
+			data[i].Link + '" target="_blank">Check it out!</a> ' +
+			'</td>' +
+			'</tr>');
 
-  }
+	}
 
 }
 
 
+function buttonHandler(evt) {
+	evt.preventDefault();
+	let buttonHref = evt.target.getAttribute('href');
+	let resourceList = document.querySelectorAll("tr td:nth-child(3)");
+	resourceList.forEach((resource, index) => {
+		if (resource.id === buttonHref.substring(1)) {
+			$("tbody tr:nth-child(" + resource.parentElement.rowIndex + ")").insertBefore("tbody tr:first");
+		}
+	});
+
+}
+let buttons = document.querySelectorAll(".resources-buttons");
+buttons.forEach(button => {
+	button.addEventListener("click", buttonHandler);
+});
 window.addEventListener('DOMContentLoaded', init);
