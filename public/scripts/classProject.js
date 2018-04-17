02b66d0b5587d5aaed52023a305475de7e7ebff2
Perrin, James <james.perrin.va@gmail.com>
2018-04-17 11:37:59 -0700

    Implemented changes to site top navigation menu.

--- public/scripts/classProject.js
+++ public/scripts/classProject.js
@@ -1,31 +1,33 @@
 //Put your JavaScript code in this file
 //master
 
-$(document).ready(function(){
-// place code inside this function
+$(document).ready(function () {
 
-//*************************************************************
-//	Hamburger icon
-//*************************************************************
-// Rename X var to something more useful
-	$(".icon").on("click", function(){
-		$(this).css("font-size", "15px");
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
-        }
-    });
-//*************************************************************
-// End of Hamburger********************************************
-//*************************************************************
+    //*************************************************************
+    //	Navigation related
+    //*************************************************************
+    $('.navbar-nav li').removeClass('active');
+    $('.navbar-nav li a').remove('span.sr-only');
 
+    if (window.location.href.indexOf("index") > -1) {
+        $('a[href^=index]').addClass('active').append('<span class="sr-only">(current)</span>');
+    }
 
+    if (window.location.href.indexOf("yearbook") > -1) {
+        $('a[href^=yearbook]').addClass('active').append('<span class="sr-only">(current)</span>');
+    }
 
+    if (window.location.href.indexOf("resources") > -1) {
+        $('a[href^=resources]').addClass('active').append('<span class="sr-only">(current)</span>');
+    }
 
+    if (window.location.href.indexOf("cssart") > -1) {
+        $('a[href^=cssart]').addClass('active').append('<span class="sr-only">(current)</span>');
+    }
 
-
+    if (window.location.href.indexOf("about") > -1) {
+        $('a[href^=about]').addClass('active').append('<span class="sr-only">(current)</span>');
+    }
 });
 
 //master
